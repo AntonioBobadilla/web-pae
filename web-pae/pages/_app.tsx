1212608333dba5dca44f2b54432887d067f4e99e
ZukiGA <gonzalezarellanobryan@gmail.com>
2022-04-04 17:18:35 -0500

    style: :art: Prettier added

--- web-pae/pages/_app.tsx
+++ web-pae/pages/_app.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import type { AppProps } from 'next/app'
-import React from 'react'
-import '../styles/globals.css'
+import type { AppProps } from 'next/app';
+import React from 'react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
