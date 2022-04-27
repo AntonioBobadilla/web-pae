90e46b100f5106016ede110e1e32b0ba34139e93
chavaxx <a01730311@tec.mx>
2022-04-27 10:28:04 -0500

    develop: added full front end of student login and modified several component files

--- web-pae/components/text-input.tsx
+++ web-pae/components/text-input.tsx
@@ -1,16 +1,17 @@
 import TextInputStyles from '../css/components/textInput.module.css';
 
-const TextInput = () => {
+const TextInput = (props: {
+  type: string | (string & {}) | undefined;
+  placeholder: string | undefined;
+}) => {
   // creo un Stateless Functional Component
 
   return (
-    <div className={TextInputStyles.textInputDiv}>
-      <input
-        className={TextInputStyles.textInput}
-        type="text"
-        placeholder="CORREO INSTITUCIONAL*"
-      ></input>
-    </div>
+    <input
+      className={TextInputStyles.textInput}
+      type={props.type}
+      placeholder={props.placeholder}
+    ></input>
   );
 };
 
