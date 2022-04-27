a0dc5ddd3743fab07ca5931d422096cf0ecfceb3
chavaxx <a01730311@tec.mx>
2022-04-27 12:57:57 -0500

    develop: finished front end and json of student login, added props of button and input components

--- web-pae/components/button-template.tsx
+++ web-pae/components/button-template.tsx
@@ -1,11 +1,26 @@
-import { ReactChild, ReactFragment, ReactPortal } from 'react';
+import {
+  MouseEventHandler,
+  ReactChild,
+  ReactFragment,
+  ReactPortal
+} from 'react';
 import ButtonTemplateStyles from '../css/components/ButtonTemplate.module.css';
 
-const ButtonTemplate = (props: { text: string }) => {
+const ButtonTemplate = (props: {
+  onClickFunction: MouseEventHandler<HTMLButtonElement> | undefined;
+  color: any | undefined;
+  text: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined;
+}) => {
   // creo un Stateless Functional Component
 
   return (
-    <button className={ButtonTemplateStyles.primaryButton}>{props.text}</button>
+    <button
+      className={ButtonTemplateStyles.primaryButton}
+      onClick={props.onClickFunction}
+      style={{ backgroundColor: props.color }}
+    >
+      {props.text}
+    </button>
   );
 };
 
