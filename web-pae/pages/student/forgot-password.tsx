e8a392b2654af355d2636bd20ab4d87877f3023c
ZukiGA <gonzalezarellanobryan@gmail.com>
2022-04-28 10:55:26 -0500

    feat: :sparkles: Style of forgotPassword page added

--- web-pae/pages/student/forgot-password.tsx
+++ web-pae/pages/student/forgot-password.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { NextPage } from 'next';
+import Link from 'next/link';
 import React from 'react';
 import ButtonTemplate from '../../components/button-template';
 import Popup from '../../components/popup';
@@ -10,13 +12,23 @@ const ForgotPassword: NextPage = () => (
   <div className={styles.container}>
     <div className={styles.background}>
       <Popup title="Recuperación de contraseña" line>
-        <form>
-          <div>
-            <span>Ingresa tu correo institucional</span>
+        <form className={styles.form}>
+          <div className={styles.input}>
+            <span className={styles.text}>Ingresa tu correo institucional</span>
             <TextInput type="text" placeholder="AXXXXXXXX@tec.mx" />
           </div>
-          <ButtonTemplate text="RECUPERAR CONTRASEÑA" />
+          <div className={styles.button}>
+            <ButtonTemplate text="RECUPERAR CONTRASEÑA" />
+          </div>
         </form>
+        <div className={styles.links}>
+          <Link href="/student/login" passHref>
+            <a>Login</a>
+          </Link>
+          <Link href="/student/login" passHref>
+            <a>Registro</a>
+          </Link>
+        </div>
       </Popup>
     </div>
   </div>
