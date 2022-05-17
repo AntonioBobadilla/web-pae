b1f32428a0bb9679115a98bbf4d5a4149771c97a
AntonioBobadilla <55169754+AntonioBobadilla@users.noreply.github.com>
2022-05-17 11:42:14 -0500

    develop: autocompleteFunction

--- web-pae/components/frontend-searchBar.tsx
+++ web-pae/components/frontend-searchBar.tsx
@@ -1,15 +1,40 @@
 import cx from 'classnames';
+import { useEffect, useState } from 'react';
 import styles from '../css/components/searchBar.module.css';
 
-const SearchBar = () => (
-  <div className={styles.searchBarInputWrapper}>
-    <i className={cx('bi bi-search', styles.icon)} />
-    <input
-      type="text"
-      className={styles.inputSearch}
-      placeholder="Busca una unidad de formación (Clave / Nombre)"
-    />
+const SearchBar = (props) =>{
+
+  const [suggestions, setSuggestions] = useState([])
+  const [inputValue, setInputValue] = useState('')
+
+  useEffect(() => {
+    setSuggestions(props.suggestions)
+  },[suggestions])
+
+
+ const handleInput = (e) => {
+     let value = e.target.value;
+     setInputValue(value);
+ } 
+
+return (
+  <div>
+    <div className={styles.searchBarInputWrapper}>
+      <i className={cx('bi bi-search', styles.icon)} />
+      <input
+        onChange={handleInput}
+        type="text"
+        className={styles.inputSearch}
+        placeholder="Busca una unidad de formación (Clave / Nombre)"
+      />
+    </div>
+    {
+      suggestions
+      .filter(materia => materia.indexOf(inputValue))
+      .map( (v,i) => { return ( <p key={i}>{suggestions[i]}</p> ) } )
+    }
   </div>
-);
+  
+)};
 
 export default SearchBar;
