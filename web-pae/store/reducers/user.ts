fffc21253ec36b1f2093d1963f9072c46c284b28
ZukiGA <gonzalezarellanobryan@gmail.com>
2022-05-19 01:03:46 -0500

    feat: :heavy_plus_sign: Configured Redux

---
+++ web-pae/store/reducers/user.ts
@@ -0,0 +1,58 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import type { RootState } from '../store';
+
+// Define a type for the slice state
+interface UserState {
+  id: string | null;
+  name: string | null;
+  email: string | null;
+  token: string | null;
+}
+
+// Define the initial state using that type
+const initialState: UserState = {
+  id: null,
+  name: null,
+  email: null,
+  token: null
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    setID: (state, action: PayloadAction<string>) => {
+      state.id = action.payload;
+    },
+    setName: (state, action: PayloadAction<string>) => {
+      state.name = action.payload;
+    },
+    setEmail: (state, action: PayloadAction<string>) => {
+      state.email = action.payload;
+    },
+    setToken: (state, action: PayloadAction<string>) => {
+      state.token = action.payload;
+    },
+    login: (state, action: PayloadAction<UserState>) => {
+      state.id = action.payload.id;
+      state.name = action.payload.name;
+      state.email = action.payload.email;
+      state.token = action.payload.token;
+    },
+    logout: (state) => {
+      state.id = null;
+      state.name = null;
+      state.email = null;
+      state.token = null;
+    }
+  }
+});
+
+export const { setID, setName, setEmail, setToken, login, logout } =
+  userSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectID = (state: RootState) => state.user.id;
+
+export default userSlice.reducer;
--- web-pae/store/store.ts
+++ web-pae/store/store.ts
@@ -1,23 +1,35 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
-import rootReducer from './reducers';
-
-// initial states here
-const initalState = {};
+import { Action } from 'redux';
+import thunk, { ThunkAction } from 'redux-thunk';
+import user from './reducers/user';
+import scheduleTutoring from './reducers/schedule-tutoring';
+import createTutor from './reducers/create-tutor';
 
 // middleware
 const middleware = [thunk];
 
 // creating store
-export const store = createStore(
-  rootReducer,
-  initalState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+
+export const store = configureStore({
+  reducer: {
+    user,
+    scheduleTutoring,
+    createTutor
+  },
+  middleware
+});
 
 // assigning store to next wrapper
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+
 const makeStore = () => store;
 
 export const wrapper = createWrapper(makeStore);
