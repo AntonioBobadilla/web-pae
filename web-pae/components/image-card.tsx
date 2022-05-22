c816e674e47e67b193fbb73c78d12a9a09d59f57
Karen Rugerio <karenrugerio@Karens-MacBook-Pro.local>
2022-05-22 16:22:30 -0500

    develop: finished Tutor home. Closes #70

--- web-pae/components/image-card.tsx
+++ web-pae/components/image-card.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import imageCardStyles from '../css/components/imageCard.module.css';
 import { useState } from 'react';
+import Link from 'next/link';
 
-const ImageCard = (props: { image: string; description: string }) => {
+const ImageCard = (props: {
+  image: string;
+  description: string;
+  path: string;
+}) => {
   const { image } = props; //descomposición del objeto props
   const { description } = props;
+  const { path } = props;
   // creo un Stateless Functional Component
   const [isToggled, setIsToggled] = useState(false); //used to store the state of the toggle button
 
@@ -12,13 +18,17 @@ const ImageCard = (props: { image: string; description: string }) => {
     setIsToggled(!isToggled);
   };
   return (
-    <div className={imageCardStyles.box} onClick={clicked}>
-      <img
-        className={isToggled ? imageCardStyles.selected : imageCardStyles.image} //terneario
-        src={props.image}
-      />
-      <p className={imageCardStyles.description}> {props.description} </p>
-    </div>
+    <Link href={path}>
+      <div className={imageCardStyles.box} onClick={clicked}>
+        <img
+          className={
+            isToggled ? imageCardStyles.selected : imageCardStyles.image
+          } //terneario
+          src={props.image}
+        />
+        <p className={imageCardStyles.description}> {props.description} </p>
+      </div>
+    </Link>
   );
 };
 
