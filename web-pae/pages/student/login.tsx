e2ababbd850ca31e6630ffc99bf880d989b7af22
chavaxx <a01730311@tec.mx>
2022-05-24 12:45:08 -0500

    develop: :fire: :fire: :fire: :fire: admin login updated and url paths to data base changed :fire: :fire: :fire:

--- web-pae/pages/student/login.tsx
+++ web-pae/pages/student/login.tsx
@@ -4,7 +4,7 @@ import LoginForm from '../../components/login-form';
 
 const Login: NextPage = () => (
   <LoginForm
-    url="http://server-pae.azurewebsites.net/logintutee/"
+    url="http://server-pae.azurewebsites.net/login/"
     image="/images/student-login-image.jpg"
     homeRoute="/student/home"
     user="tutee"
