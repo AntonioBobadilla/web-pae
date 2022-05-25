f557936d75cd82722573c6dcbe7a283b761244c2
chavaxx <a01730311@tec.mx>
2022-05-24 19:47:08 -0500

    hotfix: sowy, didnt changed branches :sob: :sob: :sob:

--- web-pae/helpers/card-info-mock.tsx
+++ web-pae/helpers/card-info-mock.tsx
@@ -7,6 +7,24 @@ const history: History[] = [
     student: 'Karen Rugerio Armenta',
     status: 'pending'
   },
+  {
+    date: 'Martes 22 de Marzo - 3:00 PM - 4:00 PM',
+    subject: 'Programación orientada a objetos',
+    student: 'Karen Rugerio Armenta',
+    status: 'info'
+  },
+  {
+    date: 'Miercoles 23 de Marzo - 3:00 PM - 4:00 PM',
+    subject: 'Programación orientada a objetos',
+    student: 'Karen Rugerio Armenta',
+    status: 'cancel'
+  },
+  {
+    date: 'Viernes 24 de Marzo - 3:00 PM - 4:00 PM',
+    subject: 'Programación orientada a objetos',
+    student: 'Karen Rugerio Armenta',
+    status: 'confirm'
+  }
 ];
 // eslint-disable-next-line import/prefer-default-export
 export function HistoryMockService() {
