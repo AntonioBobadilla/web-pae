8c17d7cf116c30c8d4a40e90887711fff79f9009
Karen Rugerio <karenrugerio@Karens-MacBook-Pro.local>
2022-05-25 20:41:35 -0500

    develop: :grin: Developed progress bar component #23

--- web-pae/pages/student/profile.tsx
+++ web-pae/pages/student/profile.tsx
@@ -3,15 +3,16 @@ import type { NextPage } from 'next';
 import React, { ReactElement } from 'react';
 import SidebarLayout from '../../components/layouts/sidebar-layout';
 import CardInformationStu from '../../components/card-information-student';
+import ProgressBarH from '../../components/progress-bar-info';
 import Styles from '../../css/student/profile.module.css';
 
 const Profile: NextPage = () => (
   <div className={Styles.content}>
-    <ToggleMenuStudent />
     <h1>Profile</h1>
+    <div className={Styles.pb}>
+      <ProgressBarH />
+    </div>
     <div className={Styles.cardInfo}>
-      <CardInformationStu />
-      <CardInformationStu />
       <CardInformationStu />
     </div>
   </div>
