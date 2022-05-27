d909ca2a400647b6ee474991501c8e9b65565771
AntonioBobadilla <55169754+AntonioBobadilla@users.noreply.github.com>
2022-05-27 03:49:09 -0500

    develop: develop of component

--- web-pae/pages/admin/tutors.tsx
+++ web-pae/pages/admin/tutors.tsx
@@ -4,38 +4,70 @@ import SidebarLayout from '../../components/layouts/sidebar-layout';
 import styles from '@/css-admin/tutees.module.css';
 import cx from 'classnames';
 import Tabs from '@/components/tabs';
+import SearchBar from '@/components/search-bar';
+import useFetch from '@/hooks/useFetch';
+import { stringify } from 'querystring';
+
 
 const Tutorings: NextPage = () => {
-  let data = [
-    {
-      matricula: 'A01734433',
-      nombre: 'Antonio Bobadilla',
-      carrera: 'ITC',
-      materias: 'TC2005B',
-      horas: 23,
-      horarios: ''
-    },
-    {
-      matricula: 'A01732533',
-      nombre: 'Salvador Gaytán',
-      carrera: 'IMT',
-      materias: 'QF1993',
-      horas: 3,
-      horarios: ''
-    },
-    {
-      matricula: 'A03741743',
-      nombre: 'Alex Reus',
-      carrera: 'IBT',
-      materias: 'TC2005B',
-      horas: 23,
-      horarios: ''
-    }
-  ];
+
+  const [asesores, setAsesores] = useState([]);
+  const [solicitudesAsesores, setSolicitudesAsesores] = useState([]);
+  const [data, setData] = useState([]);
+  const [flag, setFlag] = useState(false);
+
+  const removeDuplicates = (arr) => {
+    return arr.filter((item,
+      index) => arr.indexOf(item) === index);
+  }
+
+  const filterData = () => {
+    console.log(flag)
+    if (data != [] ){
+      data.forEach(item => {
+        if (item.is_accepted) {
+          setAsesores(asesores => [...asesores, item]);
+        } else {
+          setSolicitudesAsesores(solicitudesAsesores => [...solicitudesAsesores, item])
+        }
+      });
+      console.log(flag)
+      setFlag(true)
+    } 
+  }
+
+  const getTuteesFromApi = () => {
+    fetch('http://server-pae.azurewebsites.net/tutor/')
+    .then((resp) => resp.json())
+    .then(function(data) {
+      //console.log(data)
+      setData(data);
+      })
+    .catch(function(error) {
+      console.log(error);
+    });
+  } 
+
+
+
+  const createDataArray = () => {
+    //console.log(solicitudesAsesores);
+  } 
+
   const [currentTab, setCurrentTab] = useState('');
+
   useEffect(() => {
+    filterData();
+
+  }, [data]);
+
+  useEffect(() => {
+    getTuteesFromApi()
     setCurrentTab('tutors');
+    //createDataArray();
   }, []);
+
+
 
   const tutorsButton = () => {
     setCurrentTab('tutors');
@@ -44,8 +76,50 @@ const Tutorings: NextPage = () => {
   const requestButton = () => {
     setCurrentTab('request');
   };
+
+  const acceptTutee = (id) => {
+    fetch('http://server-pae.azurewebsites.net/tutorisaccepted/'+id, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({"is_accepted": true})
+  })
+    .then(res => {
+      if (!res.ok) { // error coming back from server
+        throw Error('could not make PUT request for that endpoint');
+      } 
+      return res.json();
+    })
+    .then(data => {
+      console.log('ok')
+    })
+    .catch(err => {
+        console.log(err.message);
+    })
+  }
+
+  const deleteTutee = (id) => {
+    fetch('http://server-pae.azurewebsites.net/tutor/'+id, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' }
+  })
+    .then(res => {
+      if (!res.ok) { // error coming back from server
+        throw Error('could not make POST request for that endpoint');
+      } 
+      return res.json();
+    })
+    .then(data => {
+      console.log('ok')
+    })
+    .catch(err => {
+        console.log(err.message);
+    })
+  }
   return (
     <div className={styles.main}>
+      <div className={styles.searchBar}>
+        <SearchBar suggestions={['a']}/>
+      </div>
       <div className={styles.tabs}>
         <div className={styles.tutorTab}>
           <Tabs
@@ -68,48 +142,98 @@ const Tutorings: NextPage = () => {
             currentTab == 'tutors' ? styles.tableTutors : styles.hidden
           }
         >
-          <tr className={cx(styles.headRow, styles.tr)}>
-            <th className={styles.head}>Matricula</th>
-            <th className={styles.head}>Nombre</th>
-            <th className={styles.head}>Carrera</th>
-            <th className={styles.head}>Materias de asesoría</th>
-            <th className={styles.head}>Número de horas</th>
-            <th className={styles.head}>Horarios</th>
-            <th className={styles.head}></th>
-          </tr>
-          {data.map(function (item) {
-            return (
-              <tr className={styles.tr}>
-                <td className={styles.td}>
-                  <p>{item.matricula}</p>
-                </td>
-                <td className={styles.td}>
-                  <p>{item.nombre}</p>
-                </td>
-                <td className={styles.td}>
-                  <p>{item.carrera}</p>
-                </td>
-                <td className={styles.td}>
-                  <p>{item.materias}</p>
-                </td>
-                <td className={styles.td}>
-                  <p>{item.horas}/80</p>
-                </td>
-                <td className={styles.td}>
-                  <button className={styles.button}>Ver</button>
-                </td>
-                <td className={styles.td}>
-                  <i className="bi bi-pencil"></i>
-                </td>
-              </tr>
-            );
-          })}
+          <thead>
+            <tr className={cx(styles.headRow, styles.tr)}>
+              <th className={styles.head}>Matricula</th>
+              <th className={styles.head}>Nombre</th>
+              <th className={styles.head}>Carrera</th>
+              <th className={styles.head}>Materias de asesoría</th>
+              <th className={styles.head}>Número de horas</th>
+              <th className={styles.head}>Horarios</th>
+              <th className={styles.head}></th>
+            </tr>
+          </thead>
+          <tbody>
+            { console.log(asesores)}
+            {
+            asesores.map(function (item,index) {
+              return (
+                <tr  key={index} className={styles.tr}>
+                  <td className={styles.td}>
+                    <p>{item.registration_number}</p>
+                  </td>
+                  <td className={styles.td}>
+                    <p>{item.name}</p>
+                  </td>
+                  <td className={styles.td}>
+                    <p>ITC</p>
+                  </td>
+                  <td className={styles.td}>
+                    <p>{item.materias}</p>
+                  </td>
+                  <td className={styles.td}>
+                    <p>{item.completed_hours}/80</p>
+                  </td>
+                  <td className={styles.td}>
+                    <button className={styles.button}>Ver</button>
+                  </td>
+                  <td className={styles.td}>
+                    <i className="bi bi-pencil"></i>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
         <table
           className={
             currentTab == 'request' ? styles.tableRequest : styles.hidden
           }
-        ></table>
+        >
+        <thead>
+          <tr className={cx(styles.headRow, styles.tr)}>
+            <th className={styles.head}>Matricula</th>
+            <th className={styles.head}>Nombre</th>
+            <th className={styles.head}>Carrera</th>
+            <th className={styles.head}>Materias de asesoría</th>
+            <th className={styles.head}>Horarios</th>
+            <th className={styles.head}>Aceptar/rechazar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+          
+          solicitudesAsesores.map(function (item,index) {
+              return (
+                <tr  key={index} className={styles.tr}>
+                  <td className={styles.td}>
+                    <p>{item.registration_number}</p>
+                  </td>
+                  <td className={styles.td}>
+                    <p>{item.name}</p>
+                  </td>
+                  <td className={styles.td}>
+                    <p>ITC</p>
+                  </td>
+                  <td className={styles.td}>
+                    <p>{item.subjects.forEach(item => {
+                      <li>{item}</li>
+                    })}</p>
+                  </td>
+                  <td className={styles.td}>
+                    <button className={styles.button}>Ver</button>
+                  </td>
+                  <td className={styles.td}>
+                    <div className={styles.flex}>
+                      <button onClick={() => acceptTutee(item.registration_number)} className={styles.accept}>Aceptar</button>
+                      <button onClick={() => deleteTutee(item.registration_number)} className={styles.denied}>Rechazar</button>
+                    </div>
+                  </td>
+                </tr>
+              );
+          })}
+        </tbody>
+        </table>
       </div>
     </div>
   );
