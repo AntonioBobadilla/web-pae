4c90f7c998eb61af0b9d73187d915d51b3775979
AntonioBobadilla <55169754+AntonioBobadilla@users.noreply.github.com>
2022-05-27 11:01:46 -0500

    fix calendar

--- web-pae/components/frontend-calendar.tsx
+++ web-pae/components/frontend-calendar.tsx
@@ -154,6 +154,46 @@ const MyCalendar = ({ eventObj, setEventObj }: MyCalendarProps) => {
           </tr>
         </thead>
         <tbody>
+        <tr>
+            <td className="titleHour">7:00 AM</td>
+            <Cell value="lunes 7am" />
+            <Cell value="martes 7am" />
+            <Cell value="miercoles 7am" />
+            <Cell value="jueves 7am" />
+            <Cell value="viernes 7am" />
+          </tr>
+          <tr>
+            <td className="titleHour">8:00 AM</td>
+            <Cell value="lunes 8am" />
+            <Cell value="martes 8am" />
+            <Cell value="miercoles 8am" />
+            <Cell value="jueves 8am" />
+            <Cell value="viernes 8am" />
+          </tr>
+          <tr className={styles.border}>
+            <td className="titleHour">9:00 AM</td>
+            <Cell value="lunes 9am" />
+            <Cell value="martes 9am" />
+            <Cell value="miercoles 9am" />
+            <Cell value="jueves 9am" />
+            <Cell value="viernes 9am" />
+          </tr>
+          <tr className={styles.border}>
+            <td className="titleHour">10:00 AM</td>
+            <Cell value="lunes 10am" />
+            <Cell value="martes 10am" />
+            <Cell value="miercoles 10am" />
+            <Cell value="jueves 10am" />
+            <Cell value="viernes 10am" />
+          </tr>
+          <tr className={styles.border}>
+            <td className="titleHour">11:00 AM</td>
+            <Cell value="lunes 11am" />
+            <Cell value="martes 11am" />
+            <Cell value="miercoles 11am" />
+            <Cell value="jueves 11am" />
+            <Cell value="viernes 11am" />
+          </tr>
         </tbody>
       </table>
     </div>
