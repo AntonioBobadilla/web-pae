939f0a2ad407defbbb80b213178c770252aa7f72
Karen Rugerio <karenrugerio@Karens-MacBook-Pro.local>
2022-05-27 03:27:08 -0500

    fix: :sparkles: Changes made to ScheduleTutoring page

--- web-pae/components/tutoring/available-tutorings.tsx
+++ web-pae/components/tutoring/available-tutorings.tsx
@@ -3,17 +3,48 @@ import { MeetingsMockService } from '../../helpers/mock-service';
 import DailyBar from '../dailybar';
 import DataTable from '../data-table/data-table';
 import { Meeting } from '../data-table/types';
+import AvailableStyles from '../../css/components/tutoring/availableTutorings.module.css';
+import AvailableTutCard from '../available-tutorings-card';
+import { access } from 'fs';
 
 const AvailableTutorings = () => {
   const [meetings, setMeetings] = React.useState<Meeting[]>(
     MeetingsMockService()
   );
   return (
-    <div>
-      <DailyBar />
-      <DataTable meetings={meetings} />
+    <div className={AvailableStyles.page}>
+      <div className={AvailableStyles.top}>
+        <span className={AvailableStyles.title}>
+          Elige el día que quieres tu asesoría
+        </span>
+      </div>
+
+      <div className={AvailableStyles.bottom}>
+        <div className={AvailableStyles.left}>
+          <div className={AvailableStyles.leftTop}>AAA</div>
+          <span className={AvailableStyles.date}> Viernes 18 de Marzo </span>
+          <div className={AvailableStyles.leftBottom}>
+            <DataTable meetings={meetings} />
+          </div>
+        </div>
+
+        <div className={AvailableStyles.right}>
+          <div className={AvailableStyles.container}>
+            <AvailableTutCard
+              date="Viernes 18 de Marzo"
+              time="12:00 pm"
+              location="Virtual"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default AvailableTutorings;
+
+/*<div className={AvailableStyles.dailybar}>
+<DailyBar />
+</div>
+*/
