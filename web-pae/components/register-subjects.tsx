9c8fc70c56fadd009d27fc892c4f08acc4282075
ZukiGA <gonzalezarellanobryan@gmail.com>
2022-06-04 12:56:19 -0500

    fix: :lipstick: Fixed register subjects responsive layout

--- web-pae/components/register-subjects.tsx
+++ web-pae/components/register-subjects.tsx
@@ -1,8 +1,8 @@
+import styles from '@/css-components/scheduleTutoring.module.css';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import ButtonTemplate from './button-template';
 import SearchBar from './search-bar';
-import styles from '@/css-components/scheduleTutoring.module.css';
 
 interface RegisterSubjectsProps {
   previousStep: () => void;
@@ -17,70 +17,78 @@ const RegisterSubjects = ({ previousStep }: RegisterSubjectsProps) => {
   const [subjectsFromApi, setSubjectsFromApi] = useState([]);
   const [valuesSelected, changeValues] = useState([]);
 
-  const handleSuggestions = (suggestion) => {
+  const handleSuggestions = (suggestion: any) => {
     changeValues((valuesSelected) => [...valuesSelected, suggestion]);
     // showValues();
   };
 
   React.useEffect(() => {
     fetch('http://localhost:3000/api/subjects')
-    .then((resp) => resp.json())
-    .then(function(data) {
-      setSubjectsFromApi(data)
-      })
-}, [])
+      .then((resp) => resp.json())
+      .then((data) => {
+        setSubjectsFromApi(data);
+      });
+  }, []);
 
-const showValues = () => {
-  const div = document.querySelector('.values');
-  cleanHTML();
-  valuesSelected.forEach((item) => {
-    const value = document.createElement('p');
-    value.innerHTML = item;
-    div.appendChild(value);
-  });
-};
+  const cleanHTML = () => {
+    const div = document.querySelector('.values');
+    while (div?.firstChild) {
+      div?.removeChild(div.firstChild);
+    }
+  };
 
-const cleanHTML = () => {
-  const div = document.querySelector('.values');
-  while (div.firstChild) {
-    div?.removeChild(div.firstChild);
-  }
-};
+  const showValues = () => {
+    const div = document.querySelector('.values');
+    cleanHTML();
+    valuesSelected.forEach((item) => {
+      const value = document.createElement('p');
+      value.innerHTML = item;
+      div?.appendChild(value);
+    });
+  };
 
-const deleteItem = (e) => {
-  const itemToDelete = e.target.parentElement.parentElement.id;
-  console.log(itemToDelete);
-  changeValues(valuesSelected.filter((item) => item != itemToDelete));
-};
+  const deleteItem = (e: any) => {
+    const itemToDelete = e.target.parentElement.parentElement.id;
+    console.log(itemToDelete);
+    changeValues(valuesSelected.filter((item) => item !== itemToDelete));
+  };
 
   return (
-    <div style={{'width':'50%'}}>
-      <div className={styles.wrapper} >
-      <SearchBar
-        function={handleSuggestions}
-        suggestions={subjectsFromApi}
-      />
+    <div className={styles.wrapper}>
+      <SearchBar function={handleSuggestions} suggestions={subjectsFromApi} />
       <div className={styles.selectedSubjects}>
-        <h2 className={styles.title}>Materia escogida</h2>
+        {valuesSelected.length !== 0 && (
+          <h2 className={styles.title}>MATERIAS SELECCIONADAS</h2>
+        )}
         <div className={styles.values}>
           {valuesSelected.map((value, index) => (
-            <p key={index} id={value} className={styles.element}>
+            <p
+              key={`${value}${index.toString()}`}
+              id={value}
+              className={styles.element}
+            >
               {value}{' '}
-              <button id={value} onClick={deleteItem} className={styles.delete}>
+              <button
+                id={value}
+                onClick={deleteItem}
+                className={styles.delete}
+                type="button"
+              >
                 <i className="bi bi-trash3" />
               </button>
             </p>
           ))}
         </div>
       </div>
+      <div className={styles.button}>
+        <ButtonTemplate
+          variant="primary"
+          onClick={() => router.push('/tutor/register-confirmation')}
+          disabled={valuesSelected.length === 0}
+        >
+          CONCLUIR REGISTRO
+        </ButtonTemplate>
       </div>
-      <ButtonTemplate
-        variant="primary"
-        onClick={() => router.push('/tutor/register-confirmation')}
-        style={{'width':'50%', 'margin':'0 auto', 'height':'40px'}}
-      >
-        CONCLUIR REGISTRO
-      </ButtonTemplate>
     </div>
   );
 };
