ec9bf5bf17e88318d4b0e0af5f5464220a83519a
chavaxx <a01730311@tec.mx>
2022-06-05 15:46:44 -0500

    develop: final style touches to addsubject

--- web-pae/components/subject-form.tsx
+++ web-pae/components/subject-form.tsx
@@ -6,13 +6,6 @@ const SubjectForm = () => {
     <div className={Styles.main}>
       <input type="text" placeholder="CLAVE*" className={Styles.input}></input>
       <input type="text" placeholder="NOMBRE*" className={Styles.input}></input>
-      <select className={Styles.select}>
-        <option value="" disabled selected>
-          CARRERA (AAA)*
-        </option>
-        <option value="ITC">ITC</option>
-      </select>
-
       <button className={Styles.button}>Agregar</button>
     </div>
   );
