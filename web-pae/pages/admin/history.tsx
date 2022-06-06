2614d83ce7ef01626f51e817164278887a9a05b0
Karen Rugerio <karenrugerio@Karens-MacBook-Pro.local>
2022-06-05 19:37:10 -0500

    subject get table

--- web-pae/pages/admin/history.tsx
+++ web-pae/pages/admin/history.tsx
@@ -7,129 +7,126 @@ import cx from 'classnames';
 import useFetch from '@/hooks/useFetch';
 
 const History: NextPage = () => {
-
   const [currentTab, setCurrentTab] = useState('');
 
   const [data, setData] = useState([]);
 
   let dummyData = [
     {
-        "id": 15,
-        "date": "2022-05-26",
-        "hour": 8,
-        "status": "PE",
-        "is_online": true,
-        "topic": "123",
-        "doubt": null,
-        "file": "http://localhost:8000/media/tutoring/Screenshot_from_2021-02-08_18-49-08.png",
-        "tutor": {
-            "registration_number": "a01731000",
-            "email": "a01731000@tec.mx",
-            "name": "Bryan G. Arellano",
-            "completed_hours": 0,
-            "is_active": true,
-            "is_accepted": false,
-            "user": "tutora01731000"
-        },
-        "student": {
-            "registration_number": "a01731097",
-            "email": "a01731097@tec.mx",
-            "name": "Salvador Gaytan putito",
-            "is_active": false,
-            "user": "studenta01731097"
-        },
-        "subject": {
-            "code": "L0173",
-            "name": "TC3004B",
-            "semester": 1
-        }
+      id: 15,
+      date: '2022-05-26',
+      hour: 8,
+      status: 'PE',
+      is_online: true,
+      topic: '123',
+      doubt: null,
+      file: 'http://localhost:8000/media/tutoring/Screenshot_from_2021-02-08_18-49-08.png',
+      tutor: {
+        registration_number: 'a01731000',
+        email: 'a01731000@tec.mx',
+        name: 'Bryan G. Arellano',
+        completed_hours: 0,
+        is_active: true,
+        is_accepted: false,
+        user: 'tutora01731000'
+      },
+      student: {
+        registration_number: 'a01731097',
+        email: 'a01731097@tec.mx',
+        name: 'Salvador Gaytan putito',
+        is_active: false,
+        user: 'studenta01731097'
+      },
+      subject: {
+        code: 'L0173',
+        name: 'TC3004B',
+        semester: 1
+      }
     },
     {
-        "id": 16,
-        "date": "2022-05-30",
-        "hour": 8,
-        "status": "PE",
-        "is_online": true,
-        "topic": "123",
-        "doubt": null,
-        "file": "http://localhost:8000/media/tutoring/Screenshot_from_2021-02-08_18-49-08.png",
-        "tutor": {
-            "registration_number": "a01731000",
-            "email": "a01731000@tec.mx",
-            "name": "Bryan G. Arellano",
-            "completed_hours": 0,
-            "is_active": true,
-            "is_accepted": false,
-            "user": "tutora01731000"
-        },
-        "student": {
-            "registration_number": "a01731097",
-            "email": "a01731097@tec.mx",
-            "name": "Karen Rugerio Armenta",
-            "is_active": false,
-            "user": "studenta01731097"
-        },
-        "subject": {
-            "code": "L0173",
-            "name": "F1004B.1",
-            "semester": 1
-        }
+      id: 16,
+      date: '2022-05-30',
+      hour: 8,
+      status: 'PE',
+      is_online: true,
+      topic: '123',
+      doubt: null,
+      file: 'http://localhost:8000/media/tutoring/Screenshot_from_2021-02-08_18-49-08.png',
+      tutor: {
+        registration_number: 'a01731000',
+        email: 'a01731000@tec.mx',
+        name: 'Bryan G. Arellano',
+        completed_hours: 0,
+        is_active: true,
+        is_accepted: false,
+        user: 'tutora01731000'
+      },
+      student: {
+        registration_number: 'a01731097',
+        email: 'a01731097@tec.mx',
+        name: 'Karen Rugerio Armenta',
+        is_active: false,
+        user: 'studenta01731097'
+      },
+      subject: {
+        code: 'L0173',
+        name: 'F1004B.1',
+        semester: 1
+      }
     },
     {
-        "id": 17,
-        "date": "2022-06-01",
-        "hour": 9,
-        "status": "PE",
-        "is_online": true,
-        "topic": "123",
-        "doubt": null,
-        "file": "http://localhost:8000/media/tutoring/Screenshot_from_2021-02-08_18-49-08_z3ZCK3o.png",
-        "tutor": {
-            "registration_number": "a01731005",
-            "email": "a01731005@tec.mx",
-            "name": "Bryan G. Arellano",
-            "completed_hours": 0,
-            "is_active": false,
-            "is_accepted": false,
-            "user": "tutora01731005"
-        },
-        "student": {
-            "registration_number": "a01731097",
-            "email": "a01731097@tec.mx",
-            "name": "José Antonio Bobadilla García",
-            "is_active": false,
-            "user": "studenta01731097"
-        },
-        "subject": {
-            "code": "L0173",
-            "name": "TC2005B",
-            "semester": 1
-        }
+      id: 17,
+      date: '2022-06-01',
+      hour: 9,
+      status: 'PE',
+      is_online: true,
+      topic: '123',
+      doubt: null,
+      file: 'http://localhost:8000/media/tutoring/Screenshot_from_2021-02-08_18-49-08_z3ZCK3o.png',
+      tutor: {
+        registration_number: 'a01731005',
+        email: 'a01731005@tec.mx',
+        name: 'Bryan G. Arellano',
+        completed_hours: 0,
+        is_active: false,
+        is_accepted: false,
+        user: 'tutora01731005'
+      },
+      student: {
+        registration_number: 'a01731097',
+        email: 'a01731097@tec.mx',
+        name: 'José Antonio Bobadilla García',
+        is_active: false,
+        user: 'studenta01731097'
+      },
+      subject: {
+        code: 'L0173',
+        name: 'TC2005B',
+        semester: 1
+      }
     }
-];
+  ];
 
   const getData = () => {
     fetch('http://server-pae.azurewebsites.net/tutoring/')
-    .then((resp) => resp.json())
-    .then(function(data) {
-      //console.log(data)
-      setData(data);
+      .then((resp) => resp.json())
+      .then(function (data) {
+        //console.log(data)
+        setData(data);
       })
-    .catch(function(error) {
-      console.log(error);
-    });
-  } 
-  
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
-    getData()
+    getData();
   }, []);
 
   return (
     <div className={history.main}>
       <button className={history.Mainbutton}>Asesorías solicitadas</button>
-      <table
-          className={styles.tableRequest}
-        >
+      <table className={styles.tableRequest}>
         <thead>
           <tr className={cx(styles.headRow, styles.tr)}>
             <th className={styles.head}>Asesor</th>
@@ -140,39 +137,40 @@ const History: NextPage = () => {
           </tr>
         </thead>
         <tbody>
-        { data.map(function (item,index) {
-          
-            let tutorName =  item.tutor != null ? item.tutor.name : "no hay tutor";
-            let studentName =  item.student != null ? item.student.name : "no hay estudiante";
-            let subjectName =  item.subject != null ? item.subject.name : "no hay materia";
-            let date =  item.date != null ? item.date : "no hay fecha";
-            let hour =  item.hour != null ? item.hour : "no hay hora";
+          {data.map(function (item, index) {
+            let tutorName =
+              item.tutor != null ? item.tutor.name : 'no hay tutor';
+            let studentName =
+              item.student != null ? item.student.name : 'no hay estudiante';
+            let subjectName =
+              item.subject != null ? item.subject.name : 'no hay materia';
+            let date = item.date != null ? item.date : 'no hay fecha';
+            let hour = item.hour != null ? item.hour : 'no hay hora';
 
-
-              return (
-                <tr  className={styles.tr}>
-                  <td className={styles.td}>
-                    <p>{tutorName}</p>
-                  </td>
-                  <td className={styles.td}>
-                    <p>{studentName}</p>
-                  </td>
-                  <td className={styles.td}>
-                    <p>{subjectName}</p>
-                  </td>
-                  <td className={styles.td}>
-                    <p>{date}</p>
-                  </td>
-                  <td className={styles.td}>
-                    <p>{hour}</p>
-                  </td>
-                </tr>
-              );
-            })}
+            return (
+              <tr className={styles.tr}>
+                <td className={styles.td}>
+                  <p>{tutorName}</p>
+                </td>
+                <td className={styles.td}>
+                  <p>{studentName}</p>
+                </td>
+                <td className={styles.td}>
+                  <p>{subjectName}</p>
+                </td>
+                <td className={styles.td}>
+                  <p>{date}</p>
+                </td>
+                <td className={styles.td}>
+                  <p>{hour}</p>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
-        </table>
+      </table>
     </div>
-  )
+  );
 };
 
 // Add sidebar layout
--- web-pae/pages/admin/subjects.tsx
+++ web-pae/pages/admin/subjects.tsx
@@ -5,6 +5,7 @@ import ImageCard from '../../components/image-card';
 import SidebarLayout from '../../components/layouts/sidebar-layout';
 import styles from '../../css/admin/subjects.module.css';
 import cx from 'classnames';
+import TutoringConfirmation from '@/components/tutoring/confirmation';
 
 const Subject = () => {
   const [currentTab, setCurrentTab] = useState('');
@@ -22,104 +23,8 @@ const Subject = () => {
 
   const [data, setData] = useState([]);
 
-  let dummyData = [
-    {
-      id: 15,
-      date: '2022-05-26',
-      hour: 8,
-      status: 'PE',
-      is_online: true,
-      topic: '123',
-      doubt: null,
-      file: 'http://localhost:8000/media/tutoring/Screenshot_from_2021-02-08_18-49-08.png',
-      tutor: {
-        registration_number: 'a01731000',
-        email: 'a01731000@tec.mx',
-        name: 'Bryan G. Arellano',
-        completed_hours: 0,
-        is_active: true,
-        is_accepted: false,
-        user: 'tutora01731000'
-      },
-      student: {
-        registration_number: 'a01731097',
-        email: 'a01731097@tec.mx',
-        name: 'Salvador Gaytan putito',
-        is_active: false,
-        user: 'studenta01731097'
-      },
-      subject: {
-        code: 'L0173',
-        name: 'TC3004B',
-        semester: 1
-      }
-    },
-    {
-      id: 16,
-      date: '2022-05-30',
-      hour: 8,
-      status: 'PE',
-      is_online: true,
-      topic: '123',
-      doubt: null,
-      file: 'http://localhost:8000/media/tutoring/Screenshot_from_2021-02-08_18-49-08.png',
-      tutor: {
-        registration_number: 'a01731000',
-        email: 'a01731000@tec.mx',
-        name: 'Bryan G. Arellano',
-        completed_hours: 0,
-        is_active: true,
-        is_accepted: false,
-        user: 'tutora01731000'
-      },
-      student: {
-        registration_number: 'a01731097',
-        email: 'a01731097@tec.mx',
-        name: 'Karen Rugerio Armenta',
-        is_active: false,
-        user: 'studenta01731097'
-      },
-      subject: {
-        code: 'L0173',
-        name: 'F1004B.1',
-        semester: 1
-      }
-    },
-    {
-      id: 17,
-      date: '2022-06-01',
-      hour: 9,
-      status: 'PE',
-      is_online: true,
-      topic: '123',
-      doubt: null,
-      file: 'http://localhost:8000/media/tutoring/Screenshot_from_2021-02-08_18-49-08_z3ZCK3o.png',
-      tutor: {
-        registration_number: 'a01731005',
-        email: 'a01731005@tec.mx',
-        name: 'Bryan G. Arellano',
-        completed_hours: 0,
-        is_active: false,
-        is_accepted: false,
-        user: 'tutora01731005'
-      },
-      student: {
-        registration_number: 'a01731097',
-        email: 'a01731097@tec.mx',
-        name: 'José Antonio Bobadilla García',
-        is_active: false,
-        user: 'studenta01731097'
-      },
-      subject: {
-        code: 'L0173',
-        name: 'TC2005B',
-        semester: 1
-      }
-    }
-  ];
-
   const getData = () => {
-    fetch('http://server-pae.azurewebsites.net/tutoring/')
+    fetch('http://server-pae.azurewebsites.net/subject/')
       .then((resp) => resp.json())
       .then(function (data) {
         //console.log(data)
@@ -160,51 +65,30 @@ const Subject = () => {
           <SubjectForm></SubjectForm>
         </div>
         <div className={currentTab == 'UF' ? styles.subjects : styles.hidden}>
-          <table className={styles.tableRequest}>
-            <thead>
-              <tr className={cx(styles.headRow, styles.tr)}>
-                <th className={styles.head}>Asesor</th>
-                <th className={styles.head}>Alumno</th>
-                <th className={styles.head}>Materia solicitada</th>
-                <th className={styles.head}>Fecha</th>
-                <th className={styles.head}>Hora</th>
-              </tr>
-            </thead>
-            <tbody>
+          <div className={styles.down}>
+            <div className={styles.tableRequest}>
+              <div className={styles.headRow}>
+                <span className={styles.clave}>Clave</span>
+                <span className={styles.name}>Nombre</span>
+                <span className={styles.edit}>Editar</span>
+                <span className={styles.delete}>Eliminar</span>
+              </div>
               {data.map(function (item, index) {
-                let tutorName =
-                  item.tutor != null ? item.tutor.name : 'no hay tutor';
-                let studentName =
-                  item.student != null
-                    ? item.student.name
-                    : 'no hay estudiante';
+                let subjectId = item.code != null ? item.code : 'no hay clave';
                 let subjectName =
-                  item.subject != null ? item.subject.name : 'no hay materia';
-                let date = item.date != null ? item.date : 'no hay fecha';
-                let hour = item.hour != null ? item.hour : 'no hay hora';
+                  item.name != null ? item.name : 'no hay nombre';
 
                 return (
-                  <tr className={styles.tr}>
-                    <td className={styles.td}>
-                      <p>{tutorName}</p>
-                    </td>
-                    <td className={styles.td}>
-                      <p>{studentName}</p>
-                    </td>
-                    <td className={styles.td}>
-                      <p>{subjectName}</p>
-                    </td>
-                    <td className={styles.td}>
-                      <p>{date}</p>
-                    </td>
-                    <td className={styles.td}>
-                      <p>{hour}</p>
-                    </td>
-                  </tr>
+                  <div className={styles.body}>
+                    <span className={styles.clave}>{subjectId}</span>
+                    <span className={styles.name}>{subjectName}</span>
+                    <span className={styles.edit}>Hola</span>
+                    <span className={styles.delete}>Hola</span>
+                  </div>
                 );
               })}
-            </tbody>
-          </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
