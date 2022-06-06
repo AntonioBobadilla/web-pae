2db35121e2939bbe3bc69e08899274f794445751
chavaxx <a01730311@tec.mx>
2022-06-06 18:22:31 -0500

    develop: edit poll finished and connected to back end :fire: :fire: :eggplant: :sweat_drops: :imp:

---
+++ web-pae/components/poll-results.tsx
@@ -0,0 +1,11 @@
+import styles from '../css/components/pollResults.module.css';
+
+const PollResults = () => {
+  return (
+    <div className={styles.main}>
+      <div></div>
+    </div>
+  );
+};
+
+export default PollResults;
