d47d820b6cad79de6dfa9446820739b9975024ca
Karen Rugerio <karenrugerio@Karens-MacBook-Pro.local>
2022-06-07 03:14:58 -0500

    Develop: :sparkles: Developed subjects show table and added search bar #215

--- web-pae/pages/admin/subjects.tsx
+++ web-pae/pages/admin/subjects.tsx
@@ -1,23 +1,36 @@
 import SubjectForm from '@/components/subject-form';
+import cx from 'classnames';
 import Tabs from '@/components/tabs';
 import React, { ReactElement, useEffect, useState } from 'react';
 import SidebarLayout from '../../components/layouts/sidebar-layout';
 import styles from '../../css/admin/subjects.module.css';
+import DeleteAdmin from '@/components/dialogs/delete-subject';
 
 const Subject = () => {
   const [currentTab, setCurrentTab] = useState('');
+  const [popUp, setPopUp] = useState(false);
+  const [id, setId] = useState(null);
+  const [data, setData] = useState([]);
+  const [pending, setPending] = useState(true);
 
   const UFButton = () => {
     setCurrentTab('UF');
+    location.reload();
   };
 
   const AddUFButton = () => {
     setCurrentTab('addUF');
   };
+  const visiblePopUp = () => {
+    setPopUp(true);
+  };
+  const checkItemState = (idItem) => {
+    setId(idItem);
+    visiblePopUp();
+  };
   useEffect(() => {
     setCurrentTab('UF');
   }, []);
-  const [data, setData] = useState([]);
 
   const getData = () => {
     fetch('http://server-pae.azurewebsites.net/subject/')
@@ -25,6 +38,7 @@ const Subject = () => {
       .then(function (data) {
         //console.log(data)
         setData(data);
+        setPending(false);
       })
       .catch(function (error) {
         console.log(error);
@@ -35,9 +49,55 @@ const Subject = () => {
     getData();
   }, []);
 
+  const notVisiblePopUp = () => {
+    setPopUp(false);
+  };
+
+  const deleteSubject = () => {
+    console.log(id);
+    fetch('http://server-pae.azurewebsites.net/subject/' + id + '/', {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then((res) => {
+        if (!res.ok) {
+          // error coming back from server
+          throw Error('could not make POST request for that endpoint');
+        } else if (res.status == 204) {
+          notVisiblePopUp();
+          getData();
+        }
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
   return (
     <div className={styles.main}>
-      <div className={styles.search}>AAA</div>
+      <div className={styles.search}>
+        <div className={styles.searchtop}>
+          <input
+            style={{
+              backgroundColor: '#F1F1F1'
+            }}
+            type="text"
+            placeholder="CLAVE*"
+            className={styles.inputclave}
+          ></input>
+          <input
+            style={{
+              backgroundColor: '#F1F1F1'
+            }}
+            type="text"
+            placeholder="NOMBRE*"
+            className={styles.inputnombre}
+          ></input>
+        </div>
+        <div className={styles.searchbottom}>
+          <button className={styles.button}>Buscar</button>
+        </div>
+      </div>
       <div className={styles.tabs}>
         <div className={styles.UfTab}>
           <Tabs
@@ -56,6 +116,7 @@ const Subject = () => {
       </div>
 
       <div className={styles.ufContainer}>
+        {pending && <div className={styles.loading}>Cargando datos...</div>}
         <div
           className={currentTab == 'addUF' ? styles.addSubject : styles.hidden}
         >
@@ -67,7 +128,6 @@ const Subject = () => {
               <div className={styles.headRow}>
                 <span className={styles.clave}>Clave</span>
                 <span className={styles.name}>Nombre</span>
-                <span className={styles.edit}>Editar</span>
                 <span className={styles.delete}>Eliminar</span>
               </div>
               {data.map(function (item, index) {
@@ -79,11 +139,19 @@ const Subject = () => {
                   <div className={styles.body}>
                     <span className={styles.clave}>{subjectId}</span>
                     <span className={styles.name}>{subjectName}</span>
-                    <span className={styles.edit}>Hola</span>
-                    <span className={styles.delete}>Hola</span>
+                    <i
+                      className={cx('bi bi-trash', styles.de)}
+                      onClick={() => checkItemState(item.code)}
+                    ></i>
                   </div>
                 );
               })}
+              <DeleteAdmin
+                visible={popUp}
+                setVisible={setPopUp}
+                onClickFunction={() => deleteSubject()}
+                onClickCancel={notVisiblePopUp}
+              ></DeleteAdmin>
             </div>
           </div>
         </div>
