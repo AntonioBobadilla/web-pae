b617eea3123eb4ee1bda9a26788cd5d9521ec6f1
ZukiGA <gonzalezarellanobryan@gmail.com>
2022-06-12 21:44:31 -0500

    fix: :bug: Token fixed

--- web-pae/pages/activate-account.tsx
+++ web-pae/pages/activate-account.tsx
@@ -1,38 +1,41 @@
+import ButtonTemplate from '@/components/button-template';
 import post from '@/helpers/post';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 
 const ActivateAccount = () => {
   const { query, push } = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    setTimeout(() => {
-      const { token } = query;
-      post(
-        {
-          token
-        },
-        'https://server-pae.azurewebsites.net/verifyemail/'
-      ).then(({ status, responseData }) => {
-        try {
-          const { message, role } = responseData;
-          if (status === 200 || status === 201 || status === 204) {
-            // toast success
-            toast.success(message);
-            // redirect to home
-            setTimeout(() => push(`/${role}/login`), 1000);
-          } else {
-            // toast error
-            toast.error(message);
-          }
-        } catch (err) {
-          toast.error('Algo salió mal');
+  const verifyEmail = () => {
+    const { token } = query;
+    setIsLoading(true);
+    post(
+      {
+        token
+      },
+      'https://server-pae.azurewebsites.net/verifyemail/'
+    ).then(({ status, responseData }) => {
+      try {
+        const { message, role } = responseData;
+        if (status === 200 || status === 201 || status === 204) {
+          // toast success
+          toast.success(message);
+          // redirect to home
+          setTimeout(() => push(`/${role}/login`), 1000);
+        } else {
+          // toast error
+          toast.error(message);
         }
-      });
-    }, 200);
-  }, []);
+        setIsLoading(false);
+      } catch (err) {
+        toast.error('Algo salió mal');
+        setIsLoading(false);
+      }
+    });
+  };
 
   return (
     <div>
@@ -44,8 +47,17 @@ const ActivateAccount = () => {
         width={100}
         height={33}
       />
-      <h1>Verificando cuenta</h1>
-      <h2>Espera un momento...</h2>
+
+      <ButtonTemplate
+        style={{ width: '25%', margin: 'auto', height: '35px' }}
+        variant="primary"
+        onClick={() => verifyEmail()}
+        loading={isLoading}
+        disabled={isLoading}
+      >
+        {!isLoading ? 'Verificar mi cuenta' : 'Verificando...'}
+      </ButtonTemplate>
+
       <Toaster position="top-right" reverseOrder={false} />
     </div>
   );
